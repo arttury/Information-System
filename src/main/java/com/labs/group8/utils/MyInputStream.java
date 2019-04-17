2a1d95f996c919a3e4953ba51b943ac8ca604586
User <escander7@gmail.com>
2019-04-17 20:17:10 +0300

    Created MyInputStream class which helps to work with InputStreams. The difference is that this class has close method

---
+++ src/main/java/com/labs/group8/utils/MyInputStream.java
@@ -0,0 +1,15 @@
+package com.labs.group8.utils;
+
+import java.io.FilterInputStream;
+import java.io.InputStream;
+
+public class MyInputStream extends FilterInputStream {
+
+    public MyInputStream(InputStream in) {
+        super(in);
+    }
+
+    @Override
+    public void close() {
+    }
+}
